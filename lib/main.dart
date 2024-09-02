ff96f9bfbc03294f7cb071d11d15595ed6beed69
Raka Akhmad Fauzi <akhmadfauziraka@gmail.com>
2024-09-02 13:59:13 +0700

    Connect Database

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:website_desa/dashboard.dart';
+import 'package:website_desa/firebase_options.dart';
 import 'package:website_desa/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: FirebaseOptions(
-      apiKey: "AIzaSyAIhWOBOregq9l7CPddUDiuao20zB-Hzso",
-      projectId: "flutter-web-connection-5b8e3",
-      messagingSenderId: "790943371145",
-      appId: "1:790943371145:web:6d4370a08ab84cd8c55b58",
-    ));
-  } else {
-    await Firebase.initializeApp();
-  }
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MyApp(),
   );
